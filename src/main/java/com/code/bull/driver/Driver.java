4ded4d568fadf5c3edb896648929c5834421daf3
amod kumar <singhamod02@gmail.com>
2023-07-18 19:37:02 +0530

    Initial commit

---
+++ src/main/java/com/code/bull/driver/Driver.java
@@ -0,0 +1,24 @@
+package com.code.bull.driver;
+
+import org.testng.annotations.BeforeSuite;
+
+public class Driver {
+
+    private static string browser = null;
+
+    private static string env = System.getProperty("env");
+
+    
+
+
+    @BeforeSuite
+    public void Setup(){
+
+
+    }
+
+    private void envSetup(){
+        if(env.)
+
+    }
+}
---
+++ src/main/java/example/JavaTesting.java
@@ -0,0 +1,25 @@
+package example;
+
+public class JavaTesting {
+
+    public static void main(String arg[]){
+                test();
+    }
+
+    public static void test() {
+        String name = "Amod";
+        if (name.equalsIgnoreCase("ajay")) {
+            System.out.println("Ajay chal gya");
+        } else if (name.equalsIgnoreCase("prashant")) {
+            System.out.println("Prashant chal gya");
+        } else if (name.equalsIgnoreCase("amod")) {
+            System.out.println("chal gaya amod ");
+        } else {
+            System.out.println(" har har mahadev bhai log");
+        }
+
+
+    }
+
+
+}
